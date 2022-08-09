1fdb0d6af568d4e9ffc8548c5f1c924a1fd1aa93
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2022-08-09 18:49:34 -0300

    creating an external css with webpack and multiple entry points

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,48 @@
 const { resolve } = require('path');
 const ESLintPlugin = require('eslint-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssWebpackPlugin = require('mini-css-extract-plugin');
+
+const dist = resolve(__dirname, 'dist');
 
 /** @type {import('webpack/declarations/WebpackOptions'.WebpackOptions)} */
 module.exports = {
   watch: true,
-  entry: './src/app.js',
-  plugins: [new ESLintPlugin()],
+  // entry: './src/app.js',
+  entry: {
+    app: './src/app.js',
+  },
+  plugins: [
+    new ESLintPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+    new MiniCssWebpackPlugin({
+      filename: 'css/style.css',
+    }),
+  ],
   output: {
-    filename: 'bundle.js',
-    path: resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    path: dist,
   },
   mode: 'development',
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: [{ loader: MiniCssWebpackPlugin.loader }, 'css-loader'],
       },
       {
         test: /\.s[ac]ss$/i,
         use: [
-          'style-loader',
+          { loader: MiniCssWebpackPlugin.loader },
           'css-loader',
           'sass-loader',
         ],
       },
     ],
+  },
+  devServer: {
+    contentBase: dist,
   },
 };
